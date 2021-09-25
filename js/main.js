10267b21b50ef8a88416a35c55e94b6e47dc864e
sondt <dinhthanhson21122000@gmail.com>
2021-09-25 10:58:34 +0700

    add cart+check-out

--- js/main.js
+++ js/main.js
@@ -239,3 +239,16 @@
     });
 
 })(jQuery);
+
+
+// Back-top
+
+$(window).on('scroll',function(){
+    var scroll=$(window).scrollTop();
+    if(scroll<400){$(".header-sticky").removeClass("sticky-bar");
+    $('#back-top').fadeOut(500);
+    }else{$(".header-sticky").addClass("sticky-bar");
+    $('#back-top').fadeIn(500);}});
+    $('#back-top a').on("click",function(){$('body,html').animate({scrollTop:0},800);return false;});
+    var menu=$('ul#navigation');
+    if(menu.length){menu.slicknav({prependTo:".mobile_menu",closedSymbol:'+',openedSymbol:'-'});};
